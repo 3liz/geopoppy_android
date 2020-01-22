d1a27bc579670cfc236bf890aa42ee98be85401e
Michael Douchin <mdouchin@3liz.com>
2020-01-22 08:47:04 +0100

    Adapt scripts to buster, improve PostgreSQL && install PureFTPd

--- conf/start.sh
+++ conf/start.sh
@@ -1,9 +1,10 @@
-#!/bin/sh 
+#!/bin/sh
 
 service postgresql start &&
 service nginx restart &&
 sudo service php7.3-fpm restart &&
-
+sudo service pure-ftpd restart &&
+#sudo redis-server /etc/redis/redis.conf &&
 qgisserver -c /storage/internal/geopoppy/conf/qgisserver.conf --rootdir /
-#wpsserver --logging debug -b 127.0.0.1 -p 8081 -w 1 --chdir /sdcard/geopoppy/processing/
-#sudo redis-server /etc/redis/redis.conf
+#wpsserver -b 127.0.0.1 -p 8081 -c /storage/internal/geopoppy/conf/wpssserver.conf
+
--- install/install.sh
+++ install/install.sh
@@ -1,44 +1,40 @@
-#!/bin/sh 
+#!/bin/sh
 
-#Nginx
+# Nginx
 cp /storage/internal/geopoppy/install/default /etc/nginx/sites-enabled/default
-        
-#PHP-FPM
+
+# PHP-FPM
 cp /storage/internal/geopoppy/install/php.ini /etc/php/7.3/fpm/php.ini
 #ln -s /usr/lib/libc-client.a /usr/lib/aarch64-linux-gnu/libc-client.a
 #ln -s /usr/lib/libc-client.a /usr/lib/arm-linux-gnueabihf/libc-client.a
 
-#start nginx +phpfpm
+# start nginx +phpfpm
 sudo service nginx start
 sudo service php7.3-fpm start
 
-#lizmap
-#lizmap_version=3.2.3
-#lizmap_git=https://github.com/3liz/lizmap-web-client.git
-#git clone --branch $lizmap_version --depth=1  $lizmap_git lizmap &&
-
-VERSION=3.3.0
+# lizmap
+lizmap_version=3.3.3
 lizmap_wps_version=master
-lizmap_wps_git=https://github.com/3liz/lizmap-wps-web-client-module.git 
+lizmap_wps_git=https://github.com/3liz/lizmap-wps-web-client-module.git
 mkdir /www
 cd /www
-wget https://github.com/3liz/lizmap-web-client/archive/$VERSION.zip
-unzip $VERSION.zip
-rm $VERSION.zip
-ln -s /www/lizmap-web-client-$VERSION/lizmap/www/ /www/lizmap
+wget https://github.com/3liz/lizmap-web-client/archive/$lizmap_version.zip
+unzip $lizmap_version.zip
+rm $lizmap_version.zip
+ln -s /www/lizmap-web-client-$lizmap_version/lizmap/www/ /www/lizmap
 
 git clone --branch $lizmap_wps_version --depth=1 $lizmap_wps_git lizmap-wps
-mv lizmap-wps/wps /www/lizmap-web-client-$VERSION/lizmap/lizmap-modules/wps
+mv lizmap-wps/wps /www/lizmap-web-client-$lizmap_version/lizmap/lizmap-modules/wps
 rm -rf lizmap-wps
 
-cd /www/lizmap-web-client-$VERSION
+cd /www/lizmap-web-client-$lizmap_version
 cp /storage/internal/geopoppy/install/lizmapConfig.ini.php lizmap/var/config/lizmapConfig.ini.php
 cp /storage/internal/geopoppy/install/localconfig.ini.php lizmap/var/config/localconfig.ini.php
 cp lizmap/var/config/profiles.ini.php.dist     lizmap/var/config/profiles.ini.php
 php lizmap/install/installer.php
 sudo sh lizmap/install/set_rights.sh www-data www-data
 sh lizmap/install/clean_vartmp.sh
-cd
+cd ~
 
 #py-qgis-server
 git_repository=https://github.com/3liz/py-qgis-server.git
@@ -54,49 +50,61 @@ wps_repository=https://github.com/3liz/py-qgis-wps.git
 api_branch=master
 api_repository=https://github.com/dmarteau/lizmap-plugin.git
 git clone --branch $api_branch --depth=1 $api_repository lizmap-plugin
-cd lizmap-plugin 
+cd lizmap-plugin
 sudo pip3 install .
 cd ..
 rm -rf lizmap-plugin
 rm -rf /root/.cache /root/.ccache
 git clone --branch $wps_branch --depth=1 $wps_repository py-qgis-wps
 sudo make -C py-qgis-wps dist
-sudo pip3 install py-qgis-wps/build/dist/*.tar.gz
-cp py-qgis-wps/factory.manifest /build.manifest
+sudo pip3 install py-qgis-wps/docker/dist/*.tar.gz
 rm -rf py-qgis-wps
 rm -rf /root/.cache /root/.ccache
 
-
-
+# pure-ftpd
+# Creating an empty shell for users
+sudo ln /bin/false /bin/ftponly
+# Configuring FTP server
+sudo echo "/bin/ftponly" >> /etc/shells
+# Each user is locked in his home
+sudo echo "yes" > /etc/pure-ftpd/conf/ChrootEveryone
+# TLS
+sudo echo "1" > /etc/pure-ftpd/conf/TLS
+# Configure the properties of directories and files created by users
+sudo echo "133 022" > /etc/pure-ftpd/conf/Umask
+# The port range for passive mode (opening outwards)
+sudo echo "5400 5600" > /etc/pure-ftpd/conf/PassivePortRange
+# Creating an SSL certificate for FTP
+sudo rm -f /etc/ssl/private/pure-ftpd.pem /etc/ssl/private/pure-ftpd.pem
+sudo openssl req -x509 -nodes -newkey rsa:1024 -keyout /etc/ssl/private/pure-ftpd.pem -out /etc/ssl/private/pure-ftpd.pem
+sudo chmod 400 /etc/ssl/private/pure-ftpd.pem
+# Restart FTP server
+sudo service pure-ftpd restart
+# Create user
+sudo groupadd ftpgroup
+sudo useradd -g ftpgroup -d /dev/null -s /etc ftpuser
+echo "geopoppy" > /tmp/pureftpd.passwd
+echo "geopoppy" >> /tmp/pureftpd.passwd
+sudo chmod 0644 /tmp/pureftpd.passwd
+sudo pure-pw useradd geopoppy -u ftpuser -g ftpgroup -d /storage/internal/geopoppy/qgis/ -m < /tmp/pureftpd.passwd
+sudo rm /tmp/pureftpd.passwd
+sudo ln -s /etc/pure-ftpd/conf/PureDB /etc/pure-ftpd/auth/50pure
+sudo pure-pw mkdb
+sudo service pure-ftpd restart
 
 #postgresql
-cp /storage/internal/geopoppy/install/postgresql.conf /etc/postgresql/9.6/main/
-cp /storage/internal/geopoppy/install/pg_hba.conf /etc/postgresql/9.6/main/
-
-service postgresql restart
-
-      # Creating a "superuser" user
-      su - postgres
-      createuser geopoppy --superuser
-      # Modifying passwords
-      psql
-      ALTER USER postgres WITH ENCRYPTED PASSWORD 'postgres';
-      ALTER USER geopoppy WITH ENCRYPTED PASSWORD 'geopoppy';
-      \q
-      createdb template_postgis -E UTF8 -T template0;
-
-      psql template_postgis 
-      CREATE EXTENSION postgis;
-      CREATE EXTENSION hstore;
-      CREATE EXTENSION postgis_topology;
-      \q
-
-      createdb -O geopoppy -T template_postgis geopoppy;
-      psql -U postgres -d geopoppy -f /storage/internal/geopoppy/db/geopoppy.sql
-      \q
-      exit
-
-
+sudo cp /storage/internal/geopoppy/install/postgresql.conf /etc/postgresql/11/main/
+sudo cp /storage/internal/geopoppy/install/pg_hba.conf /etc/postgresql/11/main/
+sudo service postgresql restart
+sudo chmod 0600 /etc/ssl/private/ssl-cert-snakeoil.key
+sudo pg_dropcluster --stop 11 main
+sudo pg_createcluster --start --locale fr_FR.UTF-8 --lc-collate fr_FR.UTF-8 --lc-ctype fr_FR.UTF-8 -e UTF-8 --port 5432 11 main
+service postgresql start
+sudo -u postgres createuser geopoppy --superuser
+sudo -u postgres psql -d postgres -c "ALTER USER geopoppy WITH ENCRYPTED PASSWORD 'geopoppy';"
+sudo -u postgres psql -d postgres -c "ALTER USER postgres WITH ENCRYPTED PASSWORD 'postgres';"
+sudo -u postgres createdb geopoppy
+sudo -u postgres psql -d geopoppy -c "CREATE EXTENSION postgis;CREATE EXTENSION hstore;"
 
 
 
