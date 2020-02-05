b4bb7a9816a8610ad17368448a116016b007cb9f
Michael Douchin <mdouchin@3liz.com>
2020-02-05 22:18:36 +0100

    Improve start script

--- conf/start.sh
+++ conf/start.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
+if [ -z ${1+x} ]
+then
+    echo "ACTION is unset: default TO START"
+    ACTION="start"
+else
+    echo "ACTION is set"
+    ACTION=$1
+fi
+echo "$ACTION has been chosen"
+
 SERVICE="postgres"
 if pgrep "$SERVICE" >/dev/null
 then
     echo "$SERVICE is already running"
+    if [ $ACTION = "forcestart" ]
+    then
+        service postgresql restart
+    fi
 else
     echo "Start $SERVICE"
     service postgresql start
@@ -13,6 +27,7 @@ SERVICE="nginx"
 if pgrep "$SERVICE" >/dev/null
 then
     echo "$SERVICE is already running"
+    if [ $ACTION = 'forcestart' ]; then service nginx restart;  fi
 else
     echo "Start $SERVICE"
     service nginx restart
@@ -22,6 +37,7 @@ SERVICE="php"
 if pgrep "$SERVICE" >/dev/null
 then
     echo "$SERVICE is already running"
+    if [ $ACTION = 'forcestart' ]; then sudo service php7.3-fpm restart;  fi
 else
     echo "Start $SERVICE"
     sudo service php7.3-fpm restart
@@ -31,15 +47,21 @@ SERVICE="ftpd"
 if pgrep "$SERVICE" >/dev/null
 then
     echo "$SERVICE is already running"
+    if [ $ACTION = 'forcestart' ]; then sudo service pure-ftpd restart;  fi
 else
     echo "Start $SERVICE"
-    sudo service pure-ftpd stop && sudo service pure-ftpd start
+    sudo service pure-ftpd restart
 fi
 
 SERVICE="redis"
 if pgrep "$SERVICE" >/dev/null
 then
     echo "$SERVICE is running"
+    if [ $ACTION = 'forcestart' ]
+    then
+        pkill $SERVICE
+        sudo redis-server /etc/redis/redis.conf > /dev/null
+    fi
 else
     echo "Start $SERVICE"
     pkill $SERVICE
@@ -50,6 +72,11 @@ SERVICE="qgisserver"
 if pgrep "$SERVICE" >/dev/null
 then
     echo "$SERVICE is running"
+    if [ $ACTION = 'forcestart' ]
+    then
+        pkill $SERVICE
+        qgisserver -c /storage/internal/geopoppy/conf/qgisserver.conf --rootdir /
+    fi
 else
     echo "Start $SERVICE"
     pkill $SERVICE
@@ -60,9 +87,13 @@ fi
 #if pgrep "$SERVICE" >/dev/null
 #then
     #echo "$SERVICE is running"
+#    if [ $ACTION = 'forcestart' ]
+#    then
+#        pkill $SERVICE
+        #wpsserver -b 127.0.0.1 -p 8081 -c /storage/internal/geopoppy/conf/wpssserver.conf
+#    fi
 #else
     #echo "Start $SERVICE"
     #pkill $SERVICE
     #wpsserver -b 127.0.0.1 -p 8081 -c /storage/internal/geopoppy/conf/wpssserver.conf
 #fi
-
